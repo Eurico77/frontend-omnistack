47a0a05fd34a18681753db1e423b572d486d094c
Eurico77 <euricomagalhaes61@gmail.com>
2020-03-25 17:11:27 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+
 
 function App() {
-  return (
-    <h1>Inicio de projeto</h1>
-  );
+  return <h1>Inicio de projeto</h1>;
 }
 
 export default App;
---
+++ src/pages/logon/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Logon() {
+    return(
+
+        <h1>Funcionado</h1>
+
+    );
+}
